2399e727e62c7893fe25269ed0e6cdd8384b493f
Natukunda Pamela <78596562+Pamella014@users.noreply.github.com>
2026-09-10 16:48:47 +0300

    feat: add hardcoded profile page for keynote speaker John Kimani

--- speakers/urls.py
+++ speakers/urls.py
@@ -9,6 +9,7 @@ app_name = 'speakers'
 urlpatterns = [
     path('', Speakers.as_view(), name='speakers'),
     path('search/', views.speaker_search, name='speaker_search'),
+    path('john-kimani/', views.john_kimani_detail, name='john_kimani_detail'),
     path('<profile_id>/', SpeakerDetailView.as_view(), name='speaker_detail'),
     path('hitcount/', include(('hitcount.urls', 'hitcount'), namespace='hitcount')),
 ]
